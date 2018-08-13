e7f491eb7a2cf12d35c18b751e74cf4da01deee1
janisza <andrew.janiszewski@chicagobooth.edu>
2018-08-13 16:34:26 -0500

    adding payment functions

--- TravelInsurance.sol
+++ TravelInsurance.sol
@@ -31,8 +31,14 @@ contract TravelInsurance {
     event contractProposed(
         address indexed _insured,
         PolicyDetails indexed _proposedPolicy,
-        uint _value
+        uint _premiumValue
         );
+
+    event contractValidated(
+        address indexed _insurer
+        PolicyDetails indexed _validatedPolicy,
+        uint _coverageValue
+        )
 
     constructor() public {
         // initial constructor called by insurance company
@@ -42,6 +48,7 @@ contract TravelInsurance {
         insurancePolicy = PolicyDetails(false, false, false);
     }
 
+    // function for policy holder to set the travel policy they want and transfer premium
     function selectNewPolicy(uint _startDateTime, uint _endDateTime, bool _erVisitCoverage, bool _prescriptionCoverage, bool _surgeryCoverage) public payable {
         insured = msg.sender;
         startDateTime = _startDateTime;
@@ -50,16 +57,14 @@ contract TravelInsurance {
         insurancePolicy.prescriptionCoverage = _prescriptionCoverage;
         insurancePolicy.surgeryCoverage = _surgeryCoverage;
 
-        // calculate insurance policy based on selected parameters
-        uint premRequired = calcPolicyPremium(insurancePolicy);
-
         // require that funds send match required premium
-        require(msg.value == premRequired);
+        require(msg.value == calcPolicyPremium(insurancePolicy));
 
         // log details of proposed contract
         emit contractProposed(msg.sender, insurancePolicy, msg.value);
     }
 
+    // function for policy holder to add emergency info that can be accessed by hospital
     function provideEmergencyInfo(string _bloodType, string _allergies, string _conditions, uint _emergencyPhNum, string _homeCountry) public {
         require(msg.sender == insured);
         emergencyInfo.bloodType = _bloodType;
@@ -69,6 +74,17 @@ contract TravelInsurance {
         emergencyInfo.homeCountry = _homeCountry;
     }
 
+    // function for insurance company to validate policy and transfer payout funds
+    function validate() public payable {
+        // require that validator is insurance company and that sufficient funds are available to cover payout
+        require(msg.sender == owner);
+        require(msg.value == calcPolicyPayout(insurancePolicy));
+
+        // log details of validated contract
+        emit contractValidated(msg.sender, insurancePolicy, msg.value)
+    }
+
+    // function to calculate insurance premium based on selected insurance type parameters
     function calcPolicyPremium(PolicyDetails _insurancePolicy) internal view returns (uint){
 
         // premium calculated as $5 per day * trip duration * a multiplier that
@@ -97,9 +113,25 @@ contract TravelInsurance {
         return premium;
     }
 
-    function validate() public {
+    // function used to calculate maximum payout based on selected insurance policy parameters
+    function calcPolicyPayout(PolicyDetails _insurancePolicy) internal pure returns (uint){
+        uint payout = 0;
 
+        if(_insurancePolicy.erVisitCoverage == true){
+            payout += 5000;
+        }
+
+        if(_insurancePolicy.prescriptionCoverage == true){
+            payout += 1000;
+        }
+
+        if(_insurancePolicy.surgeryCoverage == true){
+            payout += 10000;
+        }
+
+        return payout;
     }
+
 
 
 
